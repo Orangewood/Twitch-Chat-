d69a1d69c6f480549dcc97ab61016e1682762ca3
orangewood <jamesdolan1989@gmail.com>
2019-09-01 20:13:10 -0400

    Added button function

--- Twitch/window.py
+++ Twitch/window.py
@@ -8,6 +8,16 @@ class TwitchBot(tk.Tk):
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
 
+        label = tk.Label(self, text='testing123')
+
+        load = Image.open("logo.png")
+        render = ImageTk.PhotoImage(load)
+        img = tk.Label(self, image=render)
+        img = tk.Button(self, image=render, text="When finished, click me to begin!", compound="top", bd=0,
+                        cursor="hand2")
+        img.image = render
+        img.pack(side="top", fill='both')
+
         container.pack(side = 'top', fill = 'both', expand = True)
 
         container.grid_rowconfigure(0, weight = 1)
@@ -16,11 +26,14 @@ class TwitchBot(tk.Tk):
         #Ditionary for the frames to be called from
         self.frames = {}
 
-        frame = StartPage(container, self)
+        for Page in (StartPage, PageOne, PageTwo):
 
-        self.frames[StartPage] = frame
+            frame = Page(container, self)
 
-        frame.grid(row = 0, column = 0, sticky ='nsew')
+            self.frames[Page] = frame
+
+            frame.grid(row = 0, column = 0, sticky ='nsew')
+
 
         self.show_frame(StartPage)
 
@@ -34,16 +47,32 @@ class TwitchBot(tk.Tk):
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
+
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text = 'testing123')
-        load = Image.open("logo.png")
-        render = ImageTk.PhotoImage(load)
-        img = tk.Label(self, image=render)
-        img = tk.Button(self, image=render, text="When finished, click me to begin!", compound="top", bd=0, cursor="hand2")
-        img.image = render
-        img.pack(side="top", fill='both')
-        #root.resizable(width=False, height=False)
-        label.pack()
+
+        button = tk.Button(self, text ='Next', command = lambda: controller.show_frame(PageOne))
+        button.pack(side = 'bottom')
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text = 'page2')
+        label.pack(side = 'bottom')
+
+        button = tk.Button(self, text='Next', command=lambda: controller.show_frame(PageTwo))
+        button.pack(side = 'bottom')
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text = 'page3')
+        label.pack(side = 'bottom')
 
 
 
